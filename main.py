4654a1d0a47ae8a65368d22fbb7a453f5fd1a96d
IgliKodra <93764062+IgliKodra@users.noreply.github.com>
2022-04-30 16:38:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import itertools
+
+class tile:
+    newid = itertools.count()
+    def __init__(self):
+        self.symbol='◙'
+        self.type=""
+        self.value=0
+        self.id = next(self.newid)
+
+def printBoard(W,H,field):
+    print("  ║",end="")
+    for i in range(W):
+        print(i,end="")
+
+    print("\n══╬",end="")
+    for i in range(W):
+        print("═",end="")
+
+    print()
+
+    for i in range(H):
+        print(str(i)+" ║",end="")
+        for j in range(W):
+            print(field[i][j].symbol, end="")
+            # print(field[i][j], end="")
+
+        print()
+
+def main(W,H):
+    # Krijon fushen e lojes dhe e mushe me objektet qe do jete tile-at e lojes
+    field=[]
+    for i in range(0, H):
+        row=[]
+        for j in range(0, W):
+            row+=[tile()]
+        field+=[list(row.copy())].copy()
+   
+    # Loop-i krysor i lojes
+    alive=True
+    while(alive):
+        printBoard(W,H,field)
+        cord=input("Input the cords: ")
+        cord.split(',')
+        field[int(cord[2])][int(cord[0])].symbol='+'
+
+if __name__=="__main__":
+    main(3,3)
